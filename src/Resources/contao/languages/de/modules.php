ad9b262d2d7f4753aead444569153cbcb738b932
Mathias Arzberger <ma@pdir.de>
2017-08-01 11:09:07 +0200

    add module language file;add module dependencies to composer

---
+++ src/Resources/contao/languages/de/modules.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2017 pdir / digital agentur
+ * @package social-feed-bundle
+ * @author Mathias Arzberger <develop@pdir.de>
+ * @license http://www.gnu.org/licences/lgpl-3.0.html LGPL
+ */
+
+/**
+ * Module translation
+ */
+
+$GLOBALS['TL_LANG']['MOD']['pdir'][0] = "pdir Apps";
+$GLOBALS['TL_LANG']['MOD']['socialFeedSetup'][0]   = "Social Feed Setup";
+$GLOBALS['TL_LANG']['MOD']['socialFeedSetup'][1]   = "Verwalten Sie hier das Social Feed Bundle";
+$GLOBALS['TL_LANG']['CTE']['socialFeedList'][0]   = "SocialFeed Listansicht";
+$GLOBALS['TL_LANG']['CTE']['socialFeedList'][1]   = "Inhaltelement um die SocialFeed Listansicht auf der gewünschten Seite zu platzieren.";
