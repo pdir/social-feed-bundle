e262e8f721d33c46a54c57a8dbe3b053373bf31f
Philipp Seibt <seibt@pdir.de>
2018-12-03 10:53:28 +0100

    update template

--- src/Resources/contao/Modules/SocialFeedSetup.php
+++ src/Resources/contao/Modules/SocialFeedSetup.php
@@ -19,7 +19,7 @@ class SocialFeedSetup extends \BackendModule
     /**
      * social-feed-bundle version
      */
-    const VERSION = '2.1.2';
+    const VERSION = '2.1.3';
 
     /**
      * Template
