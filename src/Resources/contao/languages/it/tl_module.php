5dc75928c6cd7baaad37f762b1873c6c4916416d
PaoloB <paolob@contaocms.it>
2021-04-11 14:04:36 +0200

    Italian Language
    Italian translation

---
+++ src/Resources/contao/languages/it/tl_module.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2017 pdir / digital agentur
+ * @package social-feed-bundle
+ * @author Mathias Arzberger <develop@pdir.de>
+ * @license http://www.gnu.org/licences/lgpl-3.0.html LGPL
+ */
+
+/**
+ * Module translation
+ */
+
+$GLOBALS['TL_LANG']['tl_module']['pdir_sf_settings_legend'] = 'Impostazioni del Social Feed';
+
+$GLOBALS['TL_LANG']['tl_module']['pdir_sf_text_length'] = array('Lunghezza massima del testo', 'Inserisci qui la lunghezza massima del testo dei post. Inseriere 0 per visualizzare il testo completo.');
+$GLOBALS['TL_LANG']['tl_module']['pdir_sf_enableMasonry'] = array('Attiva Masonry', 'Attiva funzione Masonry. Gli elementi vengono posizionati in una posizione ottimale in base alla posizione verticale disponibile.');
+$GLOBALS['TL_LANG']['tl_module']['column1'] = 'Colonna singola';
+$GLOBALS['TL_LANG']['tl_module']['columns2'] = 'Due colonne';
+$GLOBALS['TL_LANG']['tl_module']['columns3'] = 'Tre colonne';
+$GLOBALS['TL_LANG']['tl_module']['columns4'] = 'Quattro colonne';
+$GLOBALS['TL_LANG']['tl_module']['pdir_sf_columns'] = array('Numero di colonne', 'Inserisci il numero di colonne per la visualizzazione.');
+$GLOBALS['TL_LANG']['tl_module']['pdir_sf_enableImages'] = array('Visualizza le immagini', 'Se questa opzione è attiva, vengono visualizzate le immagini.');
---
+++ src/Resources/contao/languages/it/tl_social_feed.php
@@ -0,0 +1,44 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_social_feed']['edit'] = array('Modifica', 'Modifica');
+$GLOBALS['TL_LANG']['tl_social_feed']['delete'] = array('Elimina', 'Elimina');
+$GLOBALS['TL_LANG']['tl_social_feed']['show'] = array('Visualizza', 'Visualizza le informazioni');
+$GLOBALS['TL_LANG']['tl_social_feed']['new'] = array('Crea un nuovo account di Social Feed', '');
+
+$GLOBALS['TL_LANG']['tl_social_feed']['pdir_sf_type_legend'] = 'Configurazione Social Feed';
+$GLOBALS['TL_LANG']['tl_social_feed']['pdir_sf_account_image_legend'] = 'Configurazione dell\'immagine del profilo';
+$GLOBALS['TL_LANG']['tl_social_feed']['pdir_sf_fb_legend'] = 'Configurazione Facebook';
+$GLOBALS['TL_LANG']['tl_social_feed']['pdir_sf_fb_account'] = array("Account Facebook","Inserisci il nome della pagina Facebook così com'è nell'URL (es. contao).");
+$GLOBALS['TL_LANG']['tl_social_feed']['pdir_sf_fb_app_id'] = array("App ID","Inserisci qui l'ID app della tua app Facebook.");
+$GLOBALS['TL_LANG']['tl_social_feed']['pdir_sf_fb_app_secret'] = array("App Secret","Inserisci la chiave segreta della tua app di Facebook.");
+$GLOBALS['TL_LANG']['tl_social_feed']['pdir_sf_fb_access_token'] = array("Access Token","Inserisci il token di accesso. Le istruzioni per la generazione del token di accesso sono disponibili nella documentazione.");
+$GLOBALS['TL_LANG']['tl_social_feed']['pdir_sf_fb_news_archive'] = ['Archivio News','Seleziona l\'archivio delle news dove devono essere importati i post.'];
+$GLOBALS['TL_LANG']['tl_social_feed']['pdir_sf_fb_news_cronjob'] = ['Esecuzione del cron job','Inserisci la frequenza con cui deve essere richiamato il cron job per importare i post.'];
+$GLOBALS['TL_LANG']['tl_social_feed']['pdir_sf_fb_news_last_import_date'] = array("Ultima importazione (data): verrà compilata automaticamente","Questo campo viene compilato automaticamente e non deve essere compilato.");
+$GLOBALS['TL_LANG']['tl_social_feed']['pdir_sf_fb_news_last_import_time'] = array("Ultima importazione (ora): verrà compilata automaticamente","Questo campo viene compilato automaticamente e non deve essere compilato.");
+$GLOBALS['TL_LANG']['tl_social_feed']['pdir_sf_fb_posts'] = array("Importa solo i post dalla pagina","Se questa opzione è attivata, verranno importati solo i post sulla pagina e non i post che gli utenti hanno pubblicato sulla tua bacheca.");
+$GLOBALS['TL_LANG']['tl_social_feed']['instagram_account'] = array("Nome Profilo","Immettere il nome dell'account che dovrebbe essere visualizzato sul sito Web. Il nome dell'account non è richiesto per l'importazione");
+$GLOBALS['TL_LANG']['tl_social_feed']['instagram_account_picture'] = array("Foto del profilo","Seleziona qui un'immagine del profilo, che verrà visualizzata sul sito web.");
+$GLOBALS['TL_LANG']['tl_social_feed']['instagram_account_picture_size'] = array("Dimensione dell'immagine","Qui puoi impostare le dimensioni dell'immagine e la modalità di ridimensionamento.");
+$GLOBALS['TL_LANG']['tl_social_feed']['psf_instagramAppId'] = ['Instagram App ID', 'Inserisci l\'ID app di Instagram.'];
+$GLOBALS['TL_LANG']['tl_social_feed']['psf_instagramAppSecret'] = ['Instagram App Secret', 'Inserisci la chiave segreta dell\'app Instagram.'];
+$GLOBALS['TL_LANG']['tl_social_feed']['psf_instagramAccessToken'] = ['Instagram Access Token', 'Questo è un valore generato automaticamente che viene generato quando il modulo viene inviato.'];
+$GLOBALS['TL_LANG']['tl_social_feed']['psf_instagramRequestToken'] = ['Richiedi token di accesso', 'Attiva questa opzione per richiedere un nuovo token di accesso Instagram.'];
+$GLOBALS['TL_LANG']['tl_social_feed']['socialFeedType'] = array("Tipologia Account","Selezionare il tipo di account.");
+$GLOBALS['TL_LANG']['tl_social_feed']['no_cronjob'] = "Nessun Cronjob";
+$GLOBALS['TL_LANG']['tl_social_feed']['minutely'] = "Ogni minuto";
+$GLOBALS['TL_LANG']['tl_social_feed']['hourly'] = "Ogni ora";
+$GLOBALS['TL_LANG']['tl_social_feed']['daily'] = "Ogni giorno";
+$GLOBALS['TL_LANG']['tl_social_feed']['monthly'] = "Ogni mese";
+$GLOBALS['TL_LANG']['tl_social_feed']['weekly'] = "Ogni settimana";
+$GLOBALS['TL_LANG']['tl_social_feed']['number_posts'] = array("Numero massimo di post","Inserisci qui il numero massimo di post da importare.");
+$GLOBALS['TL_LANG']['tl_social_feed']['twitter_account'] = array("Account Twitter","Inserisci qui il nome del tuo account Twitter (senza @).");
+$GLOBALS['TL_LANG']['tl_social_feed']['search'] = array("Termine di ricerca","Invece di cercare un account specifico, puoi anche cercare un termine di ricerca. Se vengono specificati l'account e il termine di ricerca, l'account viene recuperato e il termine di ricerca viene cercato nei tweet.");
+$GLOBALS['TL_LANG']['tl_social_feed']['show_retweets'] = array("Importa anche i retweet","Se questa opzione è attivata, vengono importati anche i retweet.");
+$GLOBALS['TL_LANG']['tl_social_feed']['show_reply'] = array("Importa anche le risposte","Se questa opzione è attivata, verranno importate anche le risposte.");
+$GLOBALS['TL_LANG']['tl_social_feed']['hashtags_link'] = array("Link hashtag e menzioni","Se questa opzione è attivata, gli hashtag e le menzioni sono collegati. A tale scopo è necessario utilizzare il modello esteso.");
+$GLOBALS['TL_LANG']['tl_social_feed']['twitter_api_key'] = array("API Key","Inserisci qui la API Key.");
+$GLOBALS['TL_LANG']['tl_social_feed']['twitter_api_secret_key'] = array("API Secret Key","Inserisci qui la chiave segreta dell'API.");
+$GLOBALS['TL_LANG']['tl_social_feed']['twitter_access_token'] = array("Access Token","Inserisci qui il token di accesso.");
+$GLOBALS['TL_LANG']['tl_social_feed']['twitter_access_token_secret'] = array("Access Token Secret","Inserisci qui l\'Access Token Secret.");
+
